cc651500360461f293f01569a0270eafa8752a33
Wilkin De Los Sanros <wilkin.delossantos@csedge.org>
2020-06-20 10:58:57 -0400

    with key

--- app/src/main/java/com/example/helpcenter/MainActivity.java
+++ app/src/main/java/com/example/helpcenter/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.helpcenter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMaps();
+            }
+        });
+
+    }
+
+    public void openMaps() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/helpcenter/MapsActivity.java
+++ app/src/main/java/com/example/helpcenter/MapsActivity.java
@@ -1,15 +1,15 @@
 package com.example.helpcenter;
 
-import androidx.fragment.app.FragmentActivity;
+        import androidx.fragment.app.FragmentActivity;
 
-import android.os.Bundle;
+        import android.os.Bundle;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+        import com.google.android.gms.maps.CameraUpdateFactory;
+        import com.google.android.gms.maps.GoogleMap;
+        import com.google.android.gms.maps.OnMapReadyCallback;
+        import com.google.android.gms.maps.SupportMapFragment;
+        import com.google.android.gms.maps.model.LatLng;
+        import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
