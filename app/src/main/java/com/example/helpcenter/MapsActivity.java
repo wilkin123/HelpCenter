769cab9dfd7a358f9668011369b688c4c90dc6cd
Wilkin De Los Sanros <wilkin.delossantos@csedge.org>
2020-06-26 15:34:22 -0400

    with key

--- app/src/main/java/com/example/helpcenter/MapsActivity.java
+++ app/src/main/java/com/example/helpcenter/MapsActivity.java
@@ -197,7 +197,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
         mMap.addMarker(new MarkerOptions()
                 .position(CommonPantry)
-                .title("New York Common Pantry1")
+                .title("New York Common Pantry")
                 .snippet("Food Pantry")
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
     }
